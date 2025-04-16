e4a9134225e7721d7e361a51ba15d323e94e509e
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-04-15 23:05:46 -0400

    Some CCObject fields stuff

--- src/hooks/CCObject.cpp
+++ src/hooks/CCObject.cpp
@@ -1,16 +1,80 @@
 #include <Geode/Geode.hpp>
 #include "../../include/Utils.h"
 #include "../../include/NodeModding.h"
+#include "CCObject.h"
 #include <Geode/modify/CCObject.hpp>
+#include <queue>
 
 using namespace geode::prelude;
 using namespace AlphaUtils;
 
-class $modify(CCObject) {
-    CCObject* autorelease() {
-        if (CCNode* node = typeinfo_cast<CCNode*>(this)) {
-            NodeModding::get()->handleNode(node);
-        }
-        return CCObject::autorelease();
+static std::vector<Ref<ObjectData>> s_objectArena;
+static std::queue<uint32_t> s_freeArenaSlots;
+
+uint32_t allocateObjectData(ObjectData* data) {
+    if (!s_freeArenaSlots.empty()) {
+        uint32_t id = s_freeArenaSlots.front(); s_freeArenaSlots.pop();
+        s_objectArena[id] = data;
+        return id;
+    } else {
+        s_objectArena.push_back(data);
+        return static_cast<uint32_t>(s_objectArena.size() - 1);
     }
-};
+}
+
+void releaseObjectData(uint32_t id) {
+    if (id != 0 && id < s_objectArena.size() && s_objectArena[id]) {
+        s_objectArena[id] = nullptr;
+        s_freeArenaSlots.push(id);
+    }
+}
+
+void DummyScriptEngineProtocol::removeScriptObjectByCCObject(CCObject* pObj) {
+    releaseObjectData(pObj->m_nLuaID);
+}
+
+CCScriptEngineManager* MyCCScriptEngineManager::sharedManager() {
+    auto ret = CCScriptEngineManager::sharedManager();
+    if (!ret->m_pScriptEngine) {
+        ret->setScriptEngine(new DummyScriptEngineProtocol());
+    }
+
+    return ret;
+}
+
+void FieldCCObject::tryCreateData() {
+    if (m_nLuaID == 0) {
+        ObjectData* data = new ObjectData();
+        CCPoolManager::sharedPoolManager()->addObject(data);
+        m_nLuaID = allocateObjectData(data);
+    }
+}
+
+CCObject* FieldCCObject::autorelease() {
+    if (CCNode* node = typeinfo_cast<CCNode*>(this)) {
+        NodeModding::get()->handleNode(node);
+    }
+    tryCreateData();
+
+    return CCObject::autorelease();
+}
+
+ObjectData* FieldCCObject::getObjectData() {
+    tryCreateData();
+    if (m_nLuaID < s_objectArena.size()) {
+        return s_objectArena[m_nLuaID];
+    }
+    return nullptr;
+}
+
+CCObject* FieldCCObject::getUserObject() {
+    ObjectData* data = getObjectData();
+    return data ? data->m_object : nullptr;
+}
+
+void FieldCCObject::setUserObject(CCObject* object) {
+    ObjectData* data = getObjectData();
+    if (data && data->m_object != object) {
+        data->m_object = object;
+    }
+}
---
+++ src/hooks/CCObject.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <Geode/modify/CCObject.hpp>
+#include <Geode/modify/CCScriptEngineManager.hpp>
+#include <Geode/cocos/cocoa/CCObject.h>
+
+struct ObjectData : public cocos2d::CCObject {
+    geode::Ref<cocos2d::CCObject> m_object;
+    int m_tag;
+};
+
+class DummyScriptEngineProtocol : public cocos2d::CCScriptEngineProtocol {
+    void removeScriptObjectByCCObject(cocos2d::CCObject* pObj);
+    cocos2d::ccScriptType getScriptType() { return cocos2d::kScriptTypeNone; };
+    void removeScriptHandler(int nHandler) {};
+    int reallocateScriptHandler(int nHandler) { return -1;}
+    int executeString(const char* codes) { return -1; };
+    int executeScriptFile(const char* filename) { return -1; };
+    int executeGlobalFunction(const char* functionName) { return -1; };
+    int executeNodeEvent(cocos2d::CCNode* pNode, int nAction) { return -1; };
+    int executeMenuItemEvent(cocos2d::CCMenuItem* pMenuItem) { return -1; };
+    int executeNotificationEvent(cocos2d::CCNotificationCenter* pNotificationCenter, const char* pszName) { return -1; };
+    int executeCallFuncActionEvent(cocos2d::CCCallFunc* pAction, cocos2d::CCObject* pTarget = NULL) { return -1; };
+    int executeSchedule(int nHandler, float dt, cocos2d::CCNode* pNode = NULL) { return -1; };
+    int executeLayerTouchesEvent(cocos2d::CCLayer* pLayer, int eventType, cocos2d::CCSet *pTouches) { return -1; };
+    int executeLayerTouchEvent(cocos2d::CCLayer* pLayer, int eventType, cocos2d::CCTouch *pTouch) { return -1; };
+    int executeLayerKeypadEvent(cocos2d::CCLayer* pLayer, int eventType) { return -1; };
+    int executeAccelerometerEvent(cocos2d::CCLayer* pLayer, cocos2d::CCAcceleration* pAccelerationValue) { return -1; };
+    int executeEvent(int nHandler, const char* pEventName, cocos2d::CCObject* pEventSource = NULL, const char* pEventSourceClassName = NULL) { return -1; };
+    int executeEventWithArgs(int nHandler, cocos2d::CCArray* pArgs) { return 0; }
+    bool handleAssert(const char *msg) { return false; };
+    bool parseConfig(ConfigType type, const std::string& str) { return false; };
+};
+
+class $modify(MyCCScriptEngineManager, cocos2d::CCScriptEngineManager) {
+    static cocos2d::CCScriptEngineManager* sharedManager();
+};
+
+class $modify(FieldCCObject, cocos2d::CCObject) {
+    cocos2d::CCObject* autorelease();
+    ObjectData* getObjectData();
+    cocos2d::CCObject* getUserObject();
+    void setUserObject(cocos2d::CCObject* object);
+    void tryCreateData();
+};
